80700a0198ad0044952b37602a91056c723f280b
sylv256 <contact@sylv.gay>
2024-09-09 22:30:20 -0400

    refactor: purge everything and go back to the drawing board

--- src/hook.rs
+++ src/hook.rs
@@ -1,14 +1,1 @@
-//! A collection of Windows API hooks.
-
-#![allow(non_snake_case)]
-
-use libscemu::emu::Emu;
-
-pub fn trace_winapi_call(emu: &mut Emu, _ip_addr: u64, api_addr: u64) -> bool {
-    let name = emu.api_addr_to_name(api_addr);
-    return match name.as_str() {
-        "ExAcquireFastMutex" => false,
-        "ExReleaseFastMutex" => false,
-        _ => true,
-    }
-}
+//! A collection of emulator hooks.
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,5 @@
-use libscemu::emu64;
-
 pub mod constants;
 pub mod hook;
-pub mod util;
 
 fn main() {
-    let mut emu = emu64();
-    emu.set_maps_folder("./maps64/");
-    emu.init();
-
-    emu.load_code("./ClipSp.sys");
-
-    emu.set_rip(0x1C00BFD95, false);
-
-    emu.run(None).unwrap();
 }
